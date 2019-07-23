27e4a2992fe0931ec47cd69a8c7b7751c6b101ed
Sergey Bobrov <sbobrov85@gmail.com>
2019-07-23 08:20:01 +0500

    Added GPL licences headers + small refactoring

--- src/main/java/ru/sbobrov85/lifecontrol/utils/GUI.java
+++ src/main/java/ru/sbobrov85/lifecontrol/utils/GUI.java
@@ -1,7 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2019 sergzx
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package ru.sbobrov85.lifecontrol.utils;
 
@@ -12,15 +23,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 /**
- *
- * @author sergzx
+ * Class with JavaFX GUI methods.
+ * @author sbobrov85
  */
 public final class GUI {
+    /**
+     * Contains folder name with fxml files.
+     */
+    final private static String VIEW_FOLDER = "view";
+
+    /**
+     * Contains folder name with language bundles.
+     */
+    final private static String LANGUAGE_FOLDER = "language";
+
+    /**
+     * Contains folder with css files.
+     */
+    final private static String STYLE_FOLDER = "style";
+
     /**
      * Hide default constructor.
      */
     private GUI() {
-
+      // don't write code here...
     }
 
     /**
@@ -29,7 +55,7 @@ public final class GUI {
      * @return resource url.
      */
     private static java.net.URL getResourceUrl(final String viewName) {
-        return GUI.class.getResource("/views/" + viewName + ".fxml");
+        return GUI.class.getResource("/" + VIEW_FOLDER + "/" + viewName + ".fxml");
     }
 
     /**
@@ -42,7 +68,7 @@ public final class GUI {
         Parent root = FXMLLoader.load(
             getResourceUrl(viewName),
             ResourceBundle.getBundle(
-                "languages." + viewName
+                LANGUAGE_FOLDER + "." + viewName
             )
         );
         return root;
